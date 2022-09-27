301f9e11d29e1c965bd156b0e4506071ddd2030c
relstudiosnx <relstudiosnx@gmail.com>
2022-09-27 18:40:48 +0300

    converted parallax jquery to vanilla js

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,59 +1,63 @@
 /**
  * aThemes Blocks Parallax v2
  */
-;(function( $ ) {
+document.addEventListener("DOMContentLoaded", function() {
 
-  'use strict';
+  var parallax = document.querySelectorAll('.athemes-blocks-block-container-bg-effect-parallax');
+
+  if ( parallax.length > 0 ) {
+
+    window.addEventListener('scroll', function() {
+      botigaParallax();
+    });
+
+    window.addEventListener('resize', function() {
+      botigaParallax();
+    });
+
+    window.addEventListener('load', function() {
+      botigaParallax();
+    });
+
+  }
 
   var botigaParallax = function() {
 
-    var $window   = $(window),
-        $parallax = $('.athemes-blocks-block-container-bg-effect-parallax'),
-        winHeight = $window.height();
-
-    $parallax.each( function() {
-
-      var wrapper       = this,
-          $wrapper      = $(this),
-          wrapperRect   = wrapper.getBoundingClientRect(),
-          wrapperHeight = wrapperRect.height,
-          wrapperTop    = wrapperRect.top,
-          wrapperBottom = wrapperRect.bottom;
+    for( var i = 0; i < parallax.length; i++ ) {
+  
+      var parallaxWrapper = parallax[i],
+          wrapperRect     = parallaxWrapper.getBoundingClientRect(),
+          wrapperHeight   = wrapperRect.height,
+          wrapperTop      = wrapperRect.top,
+          wrapperBottom   = wrapperRect.bottom,
+          winHeight       = window.innerHeight;
 
       if ( wrapperTop < winHeight && wrapperBottom > 0 ) {
 
-        $wrapper.find('> .athemes-blocks-background-image').each(function () {
+        var parallaxImage = parallaxWrapper.querySelector('.athemes-blocks-background-image');
 
-          var image       = this,
-              $image      = $(this),
-              imageRect   = image.getBoundingClientRect(),
-              imageHeight = imageRect.height,
-              heightDiff  = wrapperHeight - imageHeight,
-              speed       = 0.5,
-              offset      = 0;
+        if ( ! parallaxImage ) {
+          return;
+        }
 
-          var heightRatio  = winHeight + wrapperHeight,
-              topRatio     = (wrapperTop - winHeight) * -1,
-              outsideRatio = (((topRatio / heightRatio) - 0.5) * speed) + 0.5;
+        var imageRect   = parallaxImage.getBoundingClientRect(),
+            imageHeight = imageRect.height,
+            heightDiff  = wrapperHeight - imageHeight,
+            speed       = 0.5,
+            offset      = 0;
 
-          offset = ((imageHeight + wrapperHeight) * outsideRatio) - imageHeight;
+        var heightRatio  = winHeight + wrapperHeight,
+            topRatio     = (wrapperTop - winHeight) * -1,
+            outsideRatio = (((topRatio / heightRatio) - 0.5) * speed) + 0.5;
 
-          $image.css('transform', 'translate3d(0, ' + offset + 'px, 0)');
+        offset = ((imageHeight + wrapperHeight) * outsideRatio) - imageHeight;
 
-        });
+        parallaxImage.style = 'transform: translate3d(0, ' + offset + 'px, 0);';
 
       }
 
-    });
+    }
 
   };
 
-  $(window).bind('scroll resize load', function () {
-    botigaParallax();
-  });
-
-  $(document).ready( function() {
-    botigaParallax();
-  });
-
-})( jQuery );
+});
