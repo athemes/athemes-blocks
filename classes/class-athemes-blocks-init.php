dbe383c404aeb2ee0b5b31eb776e64cc077844e7
Rodrigo Teixeira <maaprote@gmail.com>
2022-12-15 14:11:35 -0300

    better enqueue logic for main stylesheet

--- classes/class-athemes-blocks-init.php
+++ classes/class-athemes-blocks-init.php
@@ -202,6 +202,12 @@ if ( ! class_exists( 'ATBLOCKS_Init' ) ) {
                     }
                 }
 
+                // Some themes/plugins might disable the enqueue from this style. E.g the filter 'should_load_separate_core_block_assets'.
+                // In this case we need a fallback to add the style within the wp_head hook. 
+                if( ! wp_style_is( 'athemes-blocks-style' ) ) {
+                    wp_enqueue_style( 'athemes-blocks-style' );
+                }
+
                 wp_add_inline_style( 'athemes-blocks-style', $this->css );
             }
         }
